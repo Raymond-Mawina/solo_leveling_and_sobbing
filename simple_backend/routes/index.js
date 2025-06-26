f5b91550250d4e5f137dcd42c70a596a4062a679
Raymond Mawina <mawina.projects@gmail.com>
2025-06-26 12:48:41 +0200

    update rogue comment

--- simple_backend/routes/index.js
+++ simple_backend/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
+/* GET the main page. */
 router.get("/", function (req, res, next) {
   res.send("welcome to simple backend");
 });
